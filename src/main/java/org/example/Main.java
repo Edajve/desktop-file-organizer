b0c06f5479d2d189db9df82a82847a25ef1d7f1a
Dajve Echols <dajvelechols@gmail.com>
2023-10-15 23:39:23 -0500

    add main method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,17 @@
 package org.example;
 
-import org.example.src.DesktopScan;
+import org.example.src.constants.DirectoryPaths;
+import org.example.src.structure.Directory;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
-        DesktopScan deskScan = new DesktopScan();
-        deskScan.scanDesktop();
+        Path basePath = Paths.get(DirectoryPaths.ROOT_DIRECTORY);
+        Directory directory = new Directory("01-vivid-seats", 0);
+        File file = new File(String.valueOf(basePath));
+        System.out.println( directory.buildDirectoryTree(file, 0));
     }
 }
