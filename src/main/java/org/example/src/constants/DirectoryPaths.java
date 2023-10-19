ae27458bf4863e6c217115e10b6553572123ab85
Dajve Echols <dajvelechols@gmail.com>
2023-10-19 18:26:46 -0500

    re wording

--- src/main/java/org/example/src/constants/DirectoryPaths.java
+++ src/main/java/org/example/src/constants/DirectoryPaths.java
@@ -4,6 +4,6 @@ import java.io.File;
 
 public class DirectoryPaths {
     public final static String DESKTOP_PATH = File.separator + "Users" + File.separator + "dajve.echols" + File.separator + "Desktop";
-    // This should be the parent dir of where your file structure starts
+    // This should be the parent directory  of where your file structure starts
     public final static String ROOT_DIRECTORY = File.separator + "Users" + File.separator + "dajve.echols" + File.separator + "file-structure" + File.separator + "01-vivid-seats";
 }
