c2263779cd54412ce8984e0b847e22e93dee372f
Dajve Echols <dajvelechols@gmail.com>
2023-10-22 14:12:58 -0500

    adding some allowed keywords

---
+++ src/main/java/org/example/src/constants/AllowedArguments.java
@@ -0,0 +1,11 @@
+package org.example.src.constants;
+
+import java.util.Arrays;
+
+public class AllowedArguments {
+    String arguments = Arrays.toString(new String[]{
+            "exit",
+            "-open",
+            "-delete"
+    });
+}
