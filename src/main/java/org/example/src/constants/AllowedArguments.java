454e343c3928dbdae4e0af0acdad7a997cb1261a
Dajve Echols <dajvelechols@gmail.com>
2023-10-22 14:16:41 -0500

    added comments context around Allowed arguments array

--- src/main/java/org/example/src/constants/AllowedArguments.java
+++ src/main/java/org/example/src/constants/AllowedArguments.java
@@ -3,6 +3,8 @@ package org.example.src.constants;
 import java.util.Arrays;
 
 public class AllowedArguments {
+    // the -before the argument means that there are additional
+    // parameters that can be passed along with the original argument
     String arguments = Arrays.toString(new String[]{
             "exit",
             "-open",
