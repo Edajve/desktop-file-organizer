1ff426f3d38451a0f05dc93aa63bab4f1a6b1e00
Dajve Echols <dajvelechols@gmail.com>
2023-10-26 20:02:05 -0500

    add method signatures for processArguments method unit tests

---
+++ src/test/java/org/example/src/CommandLineParserTest.java
@@ -0,0 +1,21 @@
+package org.example.src;
+
+import org.junit.jupiter.api.Test;
+
+public class CommandLineParserTest {
+
+    @Test
+    public void processArguments_pass_exit_as_argument_should_end_program() {
+
+    }
+
+    @Test
+    public void processArguments_pass_single_invalid_argument_should_end_program() {
+
+    }
+
+    @Test
+    public void processArguments_pass_single_valid_argument_should_continue_program() {
+
+    }
+}
