ff2669571bdcffecd6101871385b91cb00570dc0
Dajve Echols <dajvelechols@gmail.com>
2023-10-28 01:26:14 -0500

    add logger to open class

--- src/main/java/org/example/src/operations/flagOperations/Open.java
+++ src/main/java/org/example/src/operations/flagOperations/Open.java
@@ -1,12 +1,23 @@
 package org.example.src.operations.flagOperations;
 
+import org.example.src.operations.FileOperations;
+
+import java.awt.Desktop;
+
 import java.util.List;
 
+/**
+ * This class opens a file if it specifies the file name and the application
+ * that it wants to be opened.
+ */
 public class Open {
     private List<String> arguments;
+    final private FileOperations fileOperations;
+
 
     public Open(List<String> arguments) {
         this.arguments = arguments;
+        this.fileOperations = new FileOperations();
     }
 
     public List<String> getArguments() {
