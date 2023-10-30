b7efd45638136f0a6f1d06722ce4e3bcdcd18ec0
Dajve Echols <dajvelechols@gmail.com>
2023-10-30 18:06:31 -0500

    show to move refactor

--- src/main/java/org/example/src/operations/flagOperations/FlagOperations.java
+++ src/main/java/org/example/src/operations/flagOperations/FlagOperations.java
@@ -5,14 +5,13 @@ import java.util.List;
 public class FlagOperations {
     public static void openFlag(List<String> args) {
         Open open = new Open(args);
-        System.out.println(open.getArguments());
     }
 
     public static void showFlag(List<String> args) {
-        Show show = new Show();
+        Move move = new Move();
     }
 
     public static void deleteFlag(List<String> args) {
-        Delete delete = new Delete();
+        new Delete(args).execute();
     }
 }
