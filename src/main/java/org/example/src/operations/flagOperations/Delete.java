9a87d87efbe9f5bb02d60cd49c21127678c987ff
Dajve Echols <dajvelechols@gmail.com>
2023-10-30 18:05:39 -0500

    add Delete class structure

--- src/main/java/org/example/src/operations/flagOperations/Delete.java
+++ src/main/java/org/example/src/operations/flagOperations/Delete.java
@@ -1,4 +1,23 @@
 package org.example.src.operations.flagOperations;
 
+import java.util.List;
+
 public class Delete {
+    final private List<String> arguments;
+
+    public List<String> getArguments() {
+        return this.arguments;
+    }
+
+    public void deleteIndexFromArguments(int index) {
+        this.arguments.remove(index);
+    }
+
+    public Delete(List<String> arguments) {
+        this.arguments = arguments;
+    }
+
+    public void execute() {
+
+    }
 }
