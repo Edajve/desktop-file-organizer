e2bc18e828c9ed1d7d1eed9b0590fc98bfab916b
Dajve Echols <dajvelechols@gmail.com>
2023-10-31 20:58:46 -0500

    added unit test for stripKeyWord()

---
+++ src/test/java/org/example/src/UtilityTest.java
@@ -0,0 +1,37 @@
+package org.example.src;
+
+import org.example.src.utils.Utility;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class UtilityTest {
+    Utility underTest = new Utility();
+
+    @Test
+    public void stripKeyWord_no_dashes_returns_name_with_no_prefix() {
+        // Given
+        String name = "gQk-these should all remain";
+
+        // When
+        String actual = underTest.stripKeyWord(name);
+
+        // Then
+        String expected = "these-should-all-remain";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void stripKeyWord_with_dashes_returns_name_with_no_prefix() {
+        // Given
+        String name = "gQk-these-should-all-remain";
+
+        // When
+        String actual = underTest.stripKeyWord(name);
+
+        // Then
+        String expected = "these-should-all-remain";
+        assertEquals(expected, actual);
+    }
+}
