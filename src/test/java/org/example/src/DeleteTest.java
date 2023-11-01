01c8eed8a2caecc70d0d6763d7d668c42824910e
Dajve Echols <dajvelechols@gmail.com>
2023-10-31 19:39:53 -0500

    updated unit tests

--- src/test/java/org/example/src/DeleteTest.java
+++ src/test/java/org/example/src/DeleteTest.java
@@ -11,7 +11,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DeleteTest {
     Delete underTest;
