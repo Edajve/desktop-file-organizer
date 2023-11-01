7a769c6a97e5f9a9c6ec3ce3e08b4c4c61a9c55b
Dajve Echols <dajvelechols@gmail.com>
2023-10-31 19:39:09 -0500

    changed files to ignore data structrue

--- src/main/java/org/example/src/constants/Ignore.java
+++ src/main/java/org/example/src/constants/Ignore.java
@@ -1,11 +1,15 @@
 package org.example.src.constants;
 
+import java.util.List;
+
 public class Ignore {
     public static class DirectoryName {
-        public static String CODE = "03-code";
-        public static String DS_STORE = ".DS_Store";
-        public static String LOCALIZE = ".localized";
-        public static String DONT_DELETE = "File-Organizer-Key-Word-And-Structure-Guide";
-        public static String TESTING_FOLDER = "directory-for-testing";
+        public static List<String> DIRECTORIES_TO_IGNORE = List.of(
+                "CODE",
+                ".DS_STORE",
+                "LOCALIZE",
+                "DONT_DELETE",
+                "TESTING_FOLDER"
+        );
     }
 }
