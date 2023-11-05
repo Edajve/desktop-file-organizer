1e26bbf89f6a022dd60594e2b60ee62a236a6247
Dajve Echols <dajvelechols@gmail.com>
2023-11-05 16:12:39 -0600

    moved set up methid to background steps class

--- src/test/java/org/example/integration/steps/BackGroundSteps.java
+++ src/test/java/org/example/integration/steps/BackGroundSteps.java
@@ -1,11 +1,20 @@
 package org.example.integration.steps;
 
 import io.cucumber.java.en.Given;
+import org.example.src.constants.PathConstants;
+
+import java.io.File;
 
 public class BackGroundSteps {
-    @Given("set up test")
-    public static void set_up_test() {
-        // Set up
-        System.out.println("Set up here-------");
+    private static final File desktopPath = new File(PathConstants.DESKTOP_PATH);
+    @Given("There is a desktop folder named {string}")
+    public void thereIsADesktopFolderNamed(String folderName) {
+        // create the proper directory for which all the integration tests will run in
+        if (!desktopPath.exists() || !desktopPath.isDirectory())
+            System.out.println("desktop file does not exist");
+
+        boolean directoryWasMade = new File(PathConstants.TEST_DIRECTORY_PATH).mkdir();
+        if (directoryWasMade) System.out.println("Directory created on desktop: " + PathConstants.TEST_FOLDER_NAME);
+        else System.out.println("Failed to create directory because it has already been created");
     }
 }
