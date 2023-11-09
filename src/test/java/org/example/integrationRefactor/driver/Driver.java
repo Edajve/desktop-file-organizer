d304de0f61101a9529ef763415935e073387b661
Dajve Echols <dajvelechols@gmail.com>
2023-11-08 20:49:42 -0600

    reconfigured integration test for junit 5

---
+++ src/test/java/org/example/integrationRefactor/driver/Driver.java
@@ -0,0 +1,79 @@
+package org.example.integrationRefactor.driver;
+
+import org.example.Main;
+import org.example.src.constants.PathConstants;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class Driver {
+    private static Thread mainThread;
+
+    public static void createFilesInTestDirectory(List<String> fileNames) throws IOException {
+        for (String file : fileNames) {
+            File f = new File(PathConstants.TEST_DIRECTORY_PATH + File.separator + file);
+            System.out.println(f);
+            boolean isFileCreated = f.createNewFile();
+            if (isFileCreated) System.out.println("File created successfully at path " + f);
+            else System.out.println("File NOT created successfully because it has already been created");
+        }
+    }
+
+    public static void cleanOutTestDirectory() {
+        File directory = new File(PathConstants.TEST_DIRECTORY_PATH);
+        System.out.println("Cleaning out test directory --------");
+        deleteRecursively(directory);
+    }
+
+    private static void deleteRecursively(File file) {
+        if (file.isDirectory()) {
+            // Get all files and directories within this directory
+            File[] files = file.listFiles();
+            if (files != null) {
+                for (File subfile : files) {
+                    deleteRecursively(subfile);
+                }
+            }
+        }
+        // Now delete the file or empty directory
+        boolean isDeleted = file.delete();
+        if (isDeleted) {
+            System.out.println("Deleted: " + file.getPath());
+        } else {
+            System.err.println("Could not delete: " + file.getPath());
+        }
+    }
+
+    public static void runSystemUnderTest() throws InterruptedException {
+        Thread.sleep(2000);
+        mainThread = new Thread(() -> {
+            try {
+                Main.main(new String[]{PathConstants.TEST_DIRECTORY_PATH});
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        mainThread.start();
+
+        try {
+            // Delay for 2 seconds to allow the Main program to process
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.out.println("Interrupted while waiting for file processing");
+        }
+    }
+
+    public static File[] getAllFilesFromTestDirectory() {
+        return new File(PathConstants.TEST_DIRECTORY_PATH).listFiles();
+    }
+
+    public static void createTestDirectory(String directoryName) {
+        String testDirectoryPath = PathConstants.TEST_DIRECTORY_PATH + File.separator + directoryName;
+        File file = new File(testDirectoryPath);
+        boolean wasDirectoryMade = file.mkdir();
+        if (wasDirectoryMade) System.out.println("directory: " + directoryName + "was made");
+        else System.out.println("directory: " + directoryName + " was made not made because it is already made");
+    }
+}
---
+++ src/test/java/org/example/integrationRefactor/tests/BasicTest.java
@@ -0,0 +1,13 @@
+package org.example.integrationRefactor.tests;
+
+import org.example.integrationRefactor.driver.BaseTest;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BasicTest extends BaseTest {
+    @Test
+    public void basicTest() {
+        assertTrue(true);
+    }
+}
